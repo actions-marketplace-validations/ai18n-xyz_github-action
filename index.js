d825be3cb52de91c1163d25b8f1cdfb1f3d80147
Jordan Leigh <jordanleigh@Jordans-MacBook-Pro-3.local>
2023-11-08 15:15:22 -0800

    updated version

--- index.js
+++ index.js
@@ -1,25 +1,83 @@
 const axios = require('axios');
 const core = require('@actions/core');
-const fs = require('fs');
 const FormData = require('form-data');
+const fs = require('fs');
+const parser = require('@babel/parser');
+const path = require('path');
+const SHA3 = require('crypto-js/sha3');
+const traverse = require('@babel/traverse').default;
+const { execSync } = require('child_process');
 
-const UPLOAD_STRINGS_ENDPOINT = "https://c2d1-69-38-171-2.ngrok-free.app/github-actions/upload-strings";
+// Helper function to get all files in directory recursively
+function getFiles(dir, files_) {
+  files_ = files_ || [];
+  const files = fs.readdirSync(dir);
+  for (const i in files) {
+    const name = dir + '/' + files[i];
+    if (fs.statSync(name).isDirectory() && !name.includes('node_modules')) {
+      getFiles(name, files_);
+    } else if (name.match(/\.(jsx?|tsx?)$/)) {
+      files_.push(name);
+    }
+  }
+  return files_;
+}
+
+// Helper function to extract localize function calls and create hash map
+function extractLocalizeStrings(files) {
+  let localizationMap = {};
+
+  files.forEach((file) => {
+    const code = fs.readFileSync(file, 'utf8');
+    const ast = parser.parse(code, {
+      sourceType: 'module',
+      plugins: ['jsx', 'typescript'],
+    });
+
+    traverse(ast, {
+      CallExpression(path) {
+        if (path.node.callee.name === 't') {
+          const arg = path.node.arguments[0];
+          if (arg && arg.type === 'StringLiteral') {
+            const text = arg.value;
+            const hash = SHA3(text).toString();
+            localizationMap[hash] = text;
+          }
+        }
+      },
+    });
+  });
+
+  return localizationMap;
+}
+
+// Main function to generate localization map
+function generateLocalizationMap() {
+  const files = getFiles('./src'); // Point this to your source files directory
+  const localizationMap = extractLocalizeStrings(files);
+  fs.writeFileSync('localizationMap.json', JSON.stringify(localizationMap, null, 2), 'utf8');
+}
 
 async function run() {
-  try {
-    const stringsPath = core.getInput('strings_path');
-    const targetLocales = core.getInput('target_locales');
-    const fileStream = fs.createReadStream(stringsPath, 'utf8');
+  const UPLOAD_STRINGS_ENDPOINT = "https://alwaysbcoding.ngrok.io/github-actions/upload-strings";
 
-    const ai18nProjectId   = process.env.AI18N_PROJECT_ID;
-    const ai18nUploadToken = process.env.AI18N_UPLOAD_TOKEN;
+  try {
+    generateLocalizationMap();
+
+    if (!fs.existsSync('localizationMap.json')) {
+      throw new Error('localizationMap.json file not found.');
+    }
+
+    const localizationMap = fs.createReadStream('localizationMap.json');
+
+    const blendinProjectId   = process.env.BLENDIN_PROJECT_ID;
+    const blendinUploadToken = process.env.BLENDIN_UPLOAD_TOKEN;
     const githubRepository = process.env.GITHUB_REPOSITORY;
 
     const formData = new FormData();
-    formData.append('file', fileStream);
-    formData.append('project_id', ai18nProjectId);
-    formData.append('upload_token', ai18nUploadToken);
-    formData.append('target_locale_isos', targetLocales);
+    formData.append('file', localizationMap);
+    formData.append('project_id', blendinProjectId);
+    formData.append('upload_token', blendinUploadToken);
     formData.append('github_repository', githubRepository);
 
     const response = await axios.post(UPLOAD_STRINGS_ENDPOINT, formData, {
